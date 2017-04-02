fd405a1d40b979d964b5bf35b90e229377d7cdfa
Anita <AnitaLesiak1226@interia.eu>
2017-04-02 16:54:01 +0200

    try to get description from EC2

--- example_1.js
+++ example_1.js
@@ -1,8 +1,34 @@
+var AWS = require('aws-sdk');
+AWS.config.loadFromPath('./config.json');
 var task =  function(request, callback){
 
 	var fName = request.query.fName ? request.query.fName : "missing parameter: fName";
 	var lName = request.query.lName ? request.query.lName : "missing parameter: lName";
 	callback(null, fName + " " + lName);
 }
+
+var params = {
+  DryRun: true || false,
+  Filters: [
+    {
+      Name: 'STRING_VALUE',
+      Values: [
+        'STRING_VALUE',
+        /* more items */
+      ]
+    },
+    /* more items */
+  ],
+  InstanceIds: [
+    'STRING_VALUE',
+    /* more items */
+  ],
+  MaxResults: 0,
+  NextToken: 'STRING_VALUE'
+};
+ec2.describeInstances(params, function(err, data) {
+  if (err) console.log(err, err.stack); // an error occurred
+  else     console.log(data);           // successful response
+});
 
 exports.lab = task
